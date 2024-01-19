3155b6377b4419b9e2951a31dd4f5b4927d2766c
Bkorob <baka311@yandex.ru>
2024-01-19 23:39:43 +0300

    step 7 недоделал

--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,15 +1,22 @@
 from typing import Any, Dict
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 
-class HomePageView(TemplateView):
-    template_name = 'index.html'
 
-    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'djano'
-        context['eve'] = "Hello, World!"
-        return context
+def index(request): 
+    
+    'article_id': article_id,
+    'tags': tags
+    return redirect(request, 'index.html', context={
+        )
+# class HomePageView(TemplateView):
+#     template_name = 'index.html'
+
+#     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+#         context = super().get_context_data(**kwargs)
+#         context['who'] = 'djano'
+#         context['eve'] = "Hello, World!"
+#         return context
 
 
 # def about(request):

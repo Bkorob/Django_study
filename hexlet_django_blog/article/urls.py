58737b4b12db95d51d6475362abccc98c5540b5d
Bkorob <baka311@yandex.ru>
2024-02-01 17:05:19 +0300

    Step 15

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import IndexView, article, ArticleView
+from .views import IndexView, ArticleView, ArticleFormCreateView
 
 
 urlpatterns = [
-    # path('<str:tags>/<int:article_id>', article, name='article'),
+    path('create/', ArticleFormCreateView.as_view(), name='article_create'),
     path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view(), name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.views import View
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 # Create your views here.
 
 
@@ -10,15 +11,30 @@ class IndexView(View):
     template_name = 'articles/index.html'
 
     def get(self, request, *ar, **kw):
-        articles = Article.objects.all()[:5]
+        articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html', context={'articles': articles})
 
 
-def article(request, tags, article_id):
-    return render(request, 'articles/psevdohome.html', {'tag': tags, 'art': article_id})
-
-
 class ArticleView(View):
+    # с помощью функции get_object_or_404 получаем объект и пердаем его в шаблон
     def get(self, request, *ar, **kw):
         article = get_object_or_404(Article, id=kw['id'])
         return render(request, 'articles/show.html', {'article': article})
+
+
+class ArticleFormCreateView(View):
+    #  создаем обработчик формы с двумя методами
+    def get(self, request, *ar, **kw):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+    # метод обрабатывает данные из post-запроса и сохраняет их в базу если всё норм
+    # или делает редирект на ту же страницу с передачей объекта со словарем ошибок, если не норм
+    def post(self, request, *ar, **kw):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles')
+        return render(request, 'articles/create.html', {'form': form})
+            
+            
+        
